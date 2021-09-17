d6c24fbf03a78f2dc64f840df7e3e1459a58df28
Mahmudul Hasan <17101120@uap-bd.edu>
2021-09-17 23:31:21 +0600

    css add and variable use in a tag

--- src/App.js
+++ src/App.js
@@ -2,9 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let a = 55;
+  let fatherStyle = {
+    color: 'green',
+    fontSize: '36px'
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <div className="container">
+          <h3>This is my first code in React</h3>
+          <p style={fatherStyle}>My Father age is {a}</p>
+          <p style={{
+            color: 'blue',
+            fontSize: '36px'
+
+          }}>My mother age is {a - 20}</p>
+        </div>
+
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
