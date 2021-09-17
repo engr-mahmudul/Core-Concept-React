059516f72f727759c57e86e1bde3d26e37b1139b
Mahmudul Hasan <17101120@uap-bd.edu>
2021-09-17 23:57:16 +0600

    create new component and use it

--- src/App.js
+++ src/App.js
@@ -2,40 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let a = 55;
-  let fatherStyle = {
-    color: 'green',
-    fontSize: '36px'
-
-  }
+  
 
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="container">
-          <h3>This is my first code in React</h3>
-          <p style={fatherStyle}>My Father age is {a}</p>
-          <p style={{
-            color: 'blue',
-            fontSize: '36px'
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+    </div>
+  );
+}
 
-          }}>My mother age is {a - 20}</p>
-        </div>
-
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function Person(){
+  const design = {
+    backgroundColor : 'cyan',
+    color : "black",
+    boder : "2px solid black",
+    margin: "10px"
+  }
+  return (
+    <div style={design}>
+      <h1>Name: Shakib Al Hasan</h1>
+      <h4>Profession: Crickter</h4>
     </div>
   );
 }
