166cab3bf656ea30fa8a1c9e635c64c45f243ea3
Mahmudul Hasan <17101120@uap-bd.edu>
2021-09-18 16:01:45 +0600

    component with dynamic vaules

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   
 
   return (
     <div className="App">
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
+      <Friend name="Khosru" id="120"></Friend>
+      <Friend name="Urmi" id="124"></Friend>
+      <Person name="Fahim" id="126"></Person>
+      
     </div>
   );
 }
@@ -19,8 +19,9 @@ function Person(){
   const design = {
     backgroundColor : 'cyan',
     color : "black",
-    boder : "2px solid black",
-    margin: "10px"
+    border : "3px solid red",
+    margin: "10px",
+    borderRadius: "10px"
   }
   return (
     <div style={design}>
@@ -29,5 +30,13 @@ function Person(){
     </div>
   );
 }
-
+function Friend(props){
+  console.log(props);
+  return (
+    <div className="container">
+      <h1>Name: {props.name}</h1>
+      <h4>ID: {props.id}</h4>
+    </div>
+  );
+}
 export default App;
