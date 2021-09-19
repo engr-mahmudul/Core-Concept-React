7ac9e31da603b83384c389ee5ce1366489fbf335
Mahmudul Hasan <17101120@uap-bd.edu>
2021-09-19 20:27:37 +0600

    API call with React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
-function Cinemas (props){
+function Cinemas(props) {
   return (
     <div className="container">
       <h3>Nayok: {props.nayok}</h3>
@@ -12,34 +12,59 @@ function Cinemas (props){
   );
 }
 function App() {
-  
+
 
   return (
     <div className="App">
-     <Counter></Counter>
-     <Counter></Counter>
-     <Counter></Counter>
+      <Counter></Counter>
+      <External></External>
+
     </div>
   );
 }
-function Counter(){
+
+function External() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, []);
+  
+  return (
+    <div>
+      {
+        users.map(u => <ShowUsers name = {u.name} email = {u.email}></ShowUsers>)
+      }
+    </div>
+  )
+}
+function ShowUsers (props){
+  return(
+    <div>
+      <h3>Name: {props.name}</h3>
+      <h3>Email: {props.email}</h3>
+    </div>
+  )
+}
+function Counter() {
   const [count, setCount] = useState(0)
-  const increaseFunc = () => setCount(count+1);
-  const decreaseFunc = () => setCount(count-1);
+  const increaseFunc = () => setCount(count + 1);
+  const decreaseFunc = () => setCount(count - 1);
   return (
     <div className='container'>
       <h1>Count: {count}</h1>
-      <button onClick = {increaseFunc}>Increase</button>
-      <button onClick = {decreaseFunc}>Decrease</button>
+      <button onClick={increaseFunc}>Increase</button>
+      <button onClick={decreaseFunc}>Decrease</button>
     </div>
   )
 }
 
-function Person(){
+function Person() {
   const design = {
-    backgroundColor : 'cyan',
-    color : "black",
-    border : "3px solid red",
+    backgroundColor: 'cyan',
+    color: "black",
+    border: "3px solid red",
     margin: "10px",
     borderRadius: "10px"
   }
@@ -50,7 +75,7 @@ function Person(){
     </div>
   );
 }
-function Friend(props){
+function Friend(props) {
   console.log(props);
   return (
     <div className="container">
